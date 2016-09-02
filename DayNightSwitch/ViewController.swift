cbac9b4a62102e67d926c7f35b6cc207cb0c0ada
Finn Gaida <f@fga.pw>
2016-09-02 21:32:15 +0200

    Initial Commit

---
+++ DayNightSwitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DayNightSwitch
+//
+//  Created by Finn Gaida on 02.09.16.
+//  Copyright © 2016 Finn Gaida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
